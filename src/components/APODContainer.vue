72f18364809aaddb24d55d1a3d40320fcdd682eb
Joaquin Giordano <joaquin_giordano@outlook.com>
2023-03-10 15:57:41 -0300

    Finishing styles

--- src/components/APODContainer.vue
+++ src/components/APODContainer.vue
@@ -1,18 +1,18 @@
 <script setup>
 import { ref, reactive, watch, onMounted } from 'vue';
+import moment from 'moment';
 import loadingImg from '../assets/loading.svg';
-const props = defineProps({
-  selectedDate: String,
-});
+
 let data = reactive({});
 let loading = ref(true);
 let success = ref(true);
+let selectedDate = ref(moment(new Date()).format('YYYY-MM-DD'));
 async function loadData() {
   loading.value = true;
   data.hdurl = loadingImg;
   await fetch(
     `https://api.nasa.gov/planetary/apod?api_key=Ww0Xuo2kdnZyguPsJdummwwc7aW1I4M3XboVoHeY${
-      props.selectedDate ? '&date=' + props.selectedDate : ''
+      selectedDate.value ? '&date=' + selectedDate.value : ''
     }`
   )
     .then(response => response.json())
@@ -29,7 +29,7 @@ async function loadData() {
     });
 }
 watch(
-  () => props.selectedDate,
+  () => selectedDate.value,
   async newValue => {
     const date = new Date(newValue);
     if (
@@ -48,11 +48,22 @@ onMounted(() => {
 
 <template>
   <div class="apod-container">
+    <input
+      v-model="selectedDate"
+      min="1995-06-01"
+      max="2100-01-01"
+      id="date-selector"
+      type="date"
+    />
     <div v-if="success" class="info-container">
-      <h1 class="title">{{ data.title }}</h1>
-      <span class="description">{{ data.explanation }}</span>
-      <img id="apod-img" :src="data.hdurl" />
-      <b>{{ data.date }}</b>
+      <div id="left-container">
+        <h1 class="title">{{ data.title }}</h1>
+        <span class="description">{{ data.explanation }}</span>
+      </div>
+      <div id="right-container">
+        <img id="apod-img" :src="data.hdurl" />
+        <!-- <b>{{ data.date }}</b> -->
+      </div>
     </div>
     <div class="error-message" v-else>
       <h1 class="error-title">
@@ -65,10 +76,22 @@ onMounted(() => {
 <style scoped>
 .apod-container {
   background-color: #ffffff;
-  height: calc(100vh - 100px);
+  min-height: calc(100vh - 80px);
   display: flex;
+  flex-direction: column;
   border-radius: 10px;
+  align-items: center;
 }
+
+#left-container,
+#right-container {
+  /* width: 50%; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+}
+
 .error-message {
   width: 100%;
   height: 100%;
@@ -83,8 +106,9 @@ onMounted(() => {
 .info-container {
   display: flex;
   flex-direction: column;
-  gap: 20px;
-  padding: 100px;
+  height: 100%;
+  gap: 60px;
+  padding: 40px;
   justify-content: center;
   align-items: center;
 }
@@ -97,7 +121,75 @@ onMounted(() => {
 }
 #apod-img {
   border-radius: 10px;
-  height: 350px;
+  width: 100%;
+  max-height: 600px;
+  object-fit: cover;
   /* border: 1px #1f1f1f solid; */
+}
+
+::-webkit-datetime-edit {
+  color: white;
+  padding: 14px 20px;
+  background: linear-gradient(
+    45deg,
+    rgb(161, 117, 233) 0%,
+    rgb(122, 70, 205) 100%
+  );
+}
+
+::-webkit-datetime-edit-text {
+  padding: 0 0.3em;
+}
+::-webkit-datetime-edit-text,
+::-webkit-datetime-edit-month-field,
+::-webkit-datetime-edit-day-field,
+::-webkit-datetime-edit-year-field {
+  color: white;
+}
+
+::-webkit-datetime-edit-month-field:focus,
+::-webkit-datetime-edit-day-field:focus,
+::-webkit-datetime-edit-year-field:focus {
+  background-color: rgba(163, 105, 255, 0.2);
+  border-radius: 2px;
+  color: white;
+}
+::-webkit-calendar-picker-indicator {
+  margin-right: 15px;
+  cursor: pointer;
+  filter: invert();
+}
+::-webkit-inner-spin-button {
+  display: none;
+}
+
+input[type='date'] {
+  color: white;
+  background: rgb(122, 70, 205);
+  cursor: text;
+  font-size: 1.2em;
+  border: none;
+  outline: none;
+  border-radius: 0px 0px 10px 10px;
+  flex-shrink: 0;
+  width: 60%;
+  text-align: center;
+  font-family: 'Century Gothic';
+  font-weight: bold;
+}
+@media screen and (min-width: 730px) {
+  .info-container {
+    padding: 100px;
+  }
+}
+@media screen and (min-width: 1300px) {
+  .info-container {
+    flex-direction: row;
+  }
+
+  #left-container,
+  #right-container {
+    width: 50%;
+  }
 }
 </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,23 +1,10 @@
 <script setup>
-import { ref } from 'vue';
 import APODContainer from '../components/APODContainer.vue';
-
-let selectedDate = ref(null);
 </script>
 
 <template>
   <div class="home">
-    <div class="left-bar">
-      <h2>Select a Date</h2>
-      <input
-        v-model="selectedDate"
-        min="1995-06-01"
-        max="2100-01-01"
-        id="date-selector"
-        type="date"
-      />
-    </div>
-    <APODContainer class="apod-container" :selected-date="selectedDate" />
+    <APODContainer class="apod-container" />
   </div>
 </template>
 
@@ -26,88 +13,11 @@ let selectedDate = ref(null);
   display: flex;
 }
 
-.apod-container {
-  width: 70%;
-  margin: 40px;
-  box-shadow: #cccccc 0px 0px 10px 0px;
-  /* max-height: calc(100vh - 80px); */
-}
-
-.left-bar {
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  align-items: center;
-  padding: 30px 0px;
-  height: fit-content;
-  background-color: #ffffff;
-  box-shadow: #cccccc 0px 0px 10px 0px;
-  border-radius: 10px;
-  margin: 40px;
-  width: 30%;
-}
-
-.search-button {
-  width: calc(100% - 60px);
-  color: white;
-  font-weight: bold;
-  background-color: #8036bc;
-  padding: 14px;
-  font-size: 1em;
-  border-radius: 5px;
-  border: none;
-  transition: background-color 0.2s;
-}
-.search-button:hover {
-  background-color: #8c37d6;
-  cursor: pointer;
-  transition: background-color 0.2s;
-}
-
-input[type='date'] {
-  cursor: text;
-  font-size: 1.2em;
-  border: none;
-  outline: none;
-  background-color: #eee;
-  border-radius: 5px;
-  width: calc(100% - 60px);
-  text-align: center;
-  font-family: 'Century Gothic';
-  font-weight: bold;
-  /* margin: 20px; */
-}
-::-webkit-datetime-edit {
-  padding: 14px 20px;
-  background-color: #eee;
-}
-::-webkit-datetime-edit-fields-wrapper {
-  background-color: #eee;
-}
-::-webkit-datetime-edit-text {
-  padding: 0 0.3em;
-}
-::-webkit-datetime-edit-text,
-::-webkit-datetime-edit-month-field,
-::-webkit-datetime-edit-day-field,
-::-webkit-datetime-edit-year-field {
-  color: #a2a1a1;
-  /* background-color: transparent; */
-}
-
-::-webkit-datetime-edit-month-field:focus,
-::-webkit-datetime-edit-day-field:focus,
-::-webkit-datetime-edit-year-field:focus {
-  background-color: #ab7dca;
-  border-radius: 2px;
-  color: white;
-  /* background-color: transparent; */
-}
-::-webkit-calendar-picker-indicator {
-  margin-right: 15px;
-  cursor: pointer;
-}
-::-webkit-inner-spin-button {
-  display: none;
+@media screen and (min-width: 730px) {
+  .apod-container {
+    width: 100%;
+    margin: 40px;
+    box-shadow: #cccccc 0px 0px 10px 0px;
+  }
 }
 </style>
